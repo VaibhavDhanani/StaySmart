83d0ab96e62904d5dca59d4bbd3c79d01d19f849
lakhman108 <119241790+lakhman108@users.noreply.github.com>
2024-02-25 07:00:25 +0000

    restart

--- SmartStay/home/views.py
+++ SmartStay/home/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
-# Create your views here.
+from django.contrib.auth import login, authenticate
+
+
+def index(request):
+    return render(request, 'singup.html')
+
+def signup_view(request):
+    if request.method == 'POST':
+    
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_owner = False  # Set is_owner to False for regular users
+            user.save()
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=user.username, password=raw_password)
+            login(request, user)
+            return redirect('home')  # Redirect to home or another view
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
+
+def signin_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')  # Redirect to home or another view
+    else:
+        form = AuthenticationForm()
+    return render(request, 'signin.html', {'form': form})
