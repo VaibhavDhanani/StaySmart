7db7ab43dca53854f15bbbe9735f2f2de2e64cc2
lakhman108 <119241790+lakhman108@users.noreply.github.com>
2024-02-25 16:19:50 +0000

    refactorized

--- home/urls.py
+++ home/urls.py
@@ -7,5 +7,4 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about', views.about, name='about'),
-    path('main_form', views.main_form, name='main_form'),
 ]
